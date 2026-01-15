feb8122e6c97230dbc5a78d3d7b232fca7872abd
CatJAM223 <vanasutagin47@gmail.com>
2026-01-15 14:36:52 +0300

    Add files via upload

---
+++ src/logic.py
@@ -0,0 +1,49 @@
+﻿import sys
+def solve(n: int, m: int, c: list, edges: list) -> int: #1
+    graph = [] #2
+    for _ in range(n): #2
+        graph.append([]) #3
+    for x, y in edges: #4
+        x -= 1 #5
+        y -= 1 #5
+        graph[x].append(y) #5
+        graph[y].append(x) #5
+    o = 0 #6
+    stack = [(0, -1, c[0])] #6
+    while stack: #7
+        v, pr, k = stack.pop() #8
+        if k > m: #8
+            continue #9
+        is_leaf = True #10
+        for neighbor in graph[v]: #10
+            if neighbor != pr: #11
+                is_leaf = False #12
+                next_k = k * c[neighbor] + c[neighbor] #12
+                stack.append((neighbor, v, next_k)) #12
+        if is_leaf and v != 0: #13
+            o += 1 #14
+    return o #15
+
+
+
+if __name__ == "__main__":
+    # Для быстрого ввода больших данных
+    data = sys.stdin.read().split()
+    
+    n = int(data[0])
+    m = int(data[1])
+    
+    # Чтение массива котов
+    c = list(map(int, data[2:2+n]))
+    
+    # Чтение ребер
+    edges = []
+    idx = 2 + n
+    for i in range(n-1):
+        x = int(data[idx])
+        y = int(data[idx+1])
+        edges.append((x, y))
+        idx += 2
+    
+    result = solve(n, m, c, edges)
+    print(result)
---
+++ tests/test_1.py
@@ -0,0 +1,17 @@
+﻿from unittest import TestCase
+from logic import solve
+
+
+class TestSolve(TestCase):
+
+    def test_r4(self):
+        n, m, c, edges = 0, 0, [1], []
+        expected_result = 0
+        actual_result = solve(n, m, c, edges)
+        self.assertEqual(expected_result, actual_result, "aa")
+
+    def test_r5(self):
+        n, m, c, edges = 4, 4, [2], []
+        expected_result = 0
+        actual_result = solve(n, m, c, edges)
+        self.assertEqual(expected_result, actual_result, "бб")
